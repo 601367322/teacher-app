3bf23bfa75566f0d3a84e43e2a4ecd828a299ddd
Your Name <you@example.com>
2018-07-20 19:32:53 +0800

    update

--- app/src/main/java/com/prance/teacher/features/classes/ClassesDetailActivity.kt
+++ app/src/main/java/com/prance/teacher/features/classes/ClassesDetailActivity.kt
@@ -2,8 +2,10 @@ package com.prance.teacher.features.classes
 
 import com.prance.lib.base.platform.BaseFragment
 import com.prance.lib.teacher.base.core.platform.BaseActivity
+import com.prance.teacher.features.classes.view.ClassesDetailFragment
+import com.prance.teacher.features.classes.view.ClassesFragment
 
 class ClassesDetailActivity:BaseActivity(){
 
-    override fun fragment(): BaseFragment =
+    override fun fragment(): BaseFragment = ClassesDetailFragment()
 }
--- app/src/main/java/com/prance/teacher/features/classes/view/ClassesDetailFragment.kt
+++ app/src/main/java/com/prance/teacher/features/classes/view/ClassesDetailFragment.kt
@@ -3,13 +3,13 @@ package com.prance.teacher.features.classes.view
 import android.os.Bundle
 import android.view.View
 import com.prance.lib.teacher.base.core.platform.BaseFragment
+import com.prance.teacher.R
 
 class ClassesDetailFragment :BaseFragment(){
-    override fun layoutId(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+
+    override fun layoutId(): Int = R.layout.fragment_classes_detail
 
     override fun initView(rootView: View, savedInstanceState: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 }
