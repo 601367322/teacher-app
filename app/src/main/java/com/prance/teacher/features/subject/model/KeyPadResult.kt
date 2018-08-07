5125d85c19063a9984d55e68db7728208b36dd6c
Sen <shenbingbing@tengyue360.com>
2018-08-07 19:23:35 +0800

    update

--- app/src/main/java/com/prance/teacher/features/subject/model/KeyPadResult.kt
+++ app/src/main/java/com/prance/teacher/features/subject/model/KeyPadResult.kt
@@ -2,4 +2,4 @@ package com.prance.teacher.features.subject.model
 
 import java.io.Serializable
 
-data class KeyPadResult(val clickerId: String, val answer: String, val date: Long) : Serializable
+data class KeyPadResult(val clickerId: String, val answer: String, val answerTime: Long) : Serializable
