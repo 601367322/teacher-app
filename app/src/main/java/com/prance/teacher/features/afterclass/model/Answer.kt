d3dafcde38b8930a0173f6bbe9977eb0f1e4def9
Sen <shenbingbing@tengyue360.com>
2018-08-14 18:52:29 +0800

    update

--- app/src/main/java/com/prance/teacher/features/afterclass/model/Answer.kt
+++ app/src/main/java/com/prance/teacher/features/afterclass/model/Answer.kt
@@ -1,10 +1,12 @@
 package com.prance.teacher.features.afterclass.model
 
+import java.io.Serializable
+
 /**
  *Created by rich on 2018/8/8
  */
 
-class Answer{
+class Answer : Serializable {
     var clickerId: String? = null
     var answer: String? = null
     var answerTime: Long? = null
