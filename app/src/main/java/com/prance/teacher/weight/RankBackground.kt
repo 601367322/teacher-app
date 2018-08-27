a808b9c97fcb446a8773bf0d6d35dc55f1f4fc0c
Sen <shenbingbing@tengyue360.com>
2018-08-27 11:36:10 +0800

    123

--- app/src/main/java/com/prance/teacher/weight/RankBackground.kt
+++ app/src/main/java/com/prance/teacher/weight/RankBackground.kt
@@ -15,8 +15,13 @@ import kotlinx.android.synthetic.main.layout_rank_background.view.*
 
 class RankBackground : RelativeLayout {
 
+    val animators: MutableList<Animator> = mutableListOf()
+
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         addView(getInflate(this, R.layout.layout_rank_background))
+
+        var cupsTranslationY = 0
+        var cupsTranslationYDiff = 0
 
         attrs?.let {
             val a = context.obtainStyledAttributes(it, R.styleable.RankBackground)
@@ -25,6 +30,8 @@ class RankBackground : RelativeLayout {
                 if (cupsRes != -1) {
                     cups.setImageResource(cupsRes)
                 }
+                cupsTranslationY = a.getDimensionPixelOffset(R.styleable.RankBackground_cupsTranslationY, resources.getDimensionPixelOffset(R.dimen.m200_0))
+                cupsTranslationYDiff = a.getDimensionPixelOffset(R.styleable.RankBackground_cupsTranslationYDiff, 0)
             } catch (e: Exception) {
                 e.printStackTrace()
             } finally {
@@ -34,6 +41,7 @@ class RankBackground : RelativeLayout {
 
         //背景旋转光效
         val lightAnim = AnimatorSet()
+        animators.add(lightAnim)
         val rotationAnim = ObjectAnimator.ofFloat(light, AnimUtil.ROTATION, 0F, 360F)
         rotationAnim.duration = 8000
         rotationAnim.repeatCount = Animation.INFINITE
@@ -55,7 +63,8 @@ class RankBackground : RelativeLayout {
         lightAnim.interpolator = LinearInterpolator()
 
         //奖杯效果
-        val cupAnim = ObjectAnimator.ofFloat(cups, AnimUtil.TRANSLATIONY, cups.y + resources.getDimensionPixelOffset(R.dimen.m200_0), cups.y)
+        val cupAnim = ObjectAnimator.ofFloat(cups, AnimUtil.TRANSLATIONY, cups.y + cupsTranslationY, cups.y + cupsTranslationYDiff)
+        animators.add(cupAnim)
         cupAnim.addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationStart(animation: Animator?) {
                 cups.visibility = View.VISIBLE
@@ -71,6 +80,7 @@ class RankBackground : RelativeLayout {
 
         //星星效果
         val startAnim = AnimatorSet()
+        animators.add(startAnim)
         startAnim.playTogether(createStarAnim(star1), createStarAnim(star2), createStarAnim(star3), createStarAnim(star4), createStarAnim(star5), createStarAnim(star6))
         startAnim.start()
 
@@ -80,33 +90,45 @@ class RankBackground : RelativeLayout {
         val screenWidth = resources.displayMetrics.widthPixels
         val screenHeight = resources.displayMetrics.heightPixels
 
-        createMeteorAnim(
+        animators.add(createMeteorAnim(
                 meteor1,
                 -meteorHeight.toFloat() + resources.getDimensionPixelOffset(R.dimen.m14_0),
                 resources.getDimensionPixelOffset(R.dimen.m772_0).toFloat(),
                 -meteorWidth.toFloat(),
                 screenWidth.toFloat(),
                 3000
-        )
-        createMeteorAnim(
+        ))
+        animators.add(createMeteorAnim(
                 meteor2,
                 -meteorHeight.toFloat(),
                 resources.getDimensionPixelOffset(R.dimen.m138_0).toFloat(),
                 resources.getDimensionPixelOffset(R.dimen.m1511_0) - meteorWidth.toFloat(),
                 screenWidth.toFloat(),
                 5000
-        )
-        createMeteorAnim(
+        ))
+        animators.add(createMeteorAnim(
                 meteor3,
                 resources.getDimensionPixelOffset(R.dimen.m673_0).toFloat() - meteorHeight.toFloat(),
                 screenHeight.toFloat(),
                 -meteorWidth.toFloat(),
                 resources.getDimensionPixelOffset(R.dimen.m990_0).toFloat(),
                 2000
-        )
+        ))
     }
 
-    private fun createMeteorAnim(view: View, startY: Float, endY: Float, startX: Float, endX: Float, duration: Long) {
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+
+        for (anim in animators) {
+            try {
+                anim.cancel()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun createMeteorAnim(view: View, startY: Float, endY: Float, startX: Float, endX: Float, duration: Long): AnimatorSet {
         val meteorAnimY = ObjectAnimator.ofFloat(view, AnimUtil.TRANSLATIONY, startY, endY)
         meteorAnimY.repeatCount = Animation.INFINITE
         meteorAnimY.repeatMode = ValueAnimator.RESTART
@@ -119,6 +141,8 @@ class RankBackground : RelativeLayout {
         meteorAnim.playTogether(meteorAnimX, meteorAnimY)
         meteorAnim.duration = duration
         meteorAnim.start()
+
+        return meteorAnim
     }
 
     private fun createStarAnim(view: View): ObjectAnimator {
