a0d3bd687f00327bbc692ec03f61c5d9d935ce7c
Sen <shenbingbing@tengyue360.com>
2018-09-03 13:53:34 +0800

    update

--- lib.socket/src/main/java/com/prance/lib/socket/PostMessageGenerate.kt
+++ lib.socket/src/main/java/com/prance/lib/socket/PostMessageGenerate.kt
@@ -3,8 +3,8 @@ package com.prance.lib.socket
 import com.google.gson.Gson
 import java.io.Serializable
 
-data class MessageTemp(var cmd: Int, var data: Any)
+data class MessageTemp(var cmd: Int, var data: Any) : Serializable
 
 fun generatePostMessage(cmd: Int, obj: Serializable): String {
-    return Gson().toJson(MessageTemp(cmd,obj))
+    return Gson().toJson(MessageTemp(cmd, obj))
 }
