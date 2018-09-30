26de65600b7566b1f770a46630d5d1cae43792e8
Sen <shenbingbing@tengyue360.com>
2018-09-30 11:33:20 +0800

    update

--- app/src/main/java/com/prance/teacher/features/main/view/MainFragment.kt
+++ app/src/main/java/com/prance/teacher/features/main/view/MainFragment.kt
@@ -71,9 +71,9 @@ class MainFragment : BaseFragment(), IMainContract.View {
 //                context?.let { startActivity(StudentsActivity.callingIntent(it, ClassesEntity(1))) }
 
 //            context?.let { startActivity(CheckKeyPadActivity.callingIntent(it)) }
-            context?.let { startActivity(ReplaceKeyPadActivity.callingIntent(it,ClassesEntity(1))) }
+//            context?.let { startActivity(ReplaceKeyPadActivity.callingIntent(it,ClassesEntity(1))) }
 
-//                var question = ClassesDetailFragment.Question(1, 10, "1,0,0,0,4,1", 1, "A", mutableListOf(
+//                val question = ClassesDetailFragment.Question(1, 10, "1,0,0,0,4,1", 1, "A", mutableListOf(
 //                        StudentsEntity(1,"呵呵","呵呵")
 //                ))
 //                context?.let { startActivity(SubjectActivity.callingIntent(it, question)) }
@@ -84,7 +84,7 @@ class MainFragment : BaseFragment(), IMainContract.View {
 //                context?.let {
 //                    startActivity(Intent(it,DanmuTestActivity::class.java))
 //                }
-                return@setOnClickListener
+//                return@setOnClickListener
             }
             context?.let {
                 startActivity(ClassesActivity.callingIntent(it, ClassesFragment.ACTION_TO_CLASS))
