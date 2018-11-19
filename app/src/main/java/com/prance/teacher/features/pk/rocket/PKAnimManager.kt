8485a8479cd6da8b6215e69577f7ebf69869f9b1
Sen <shenbingbing@tengyue360.com>
2018-11-19 10:10:48 +0800

    update

--- app/src/main/java/com/prance/teacher/features/pk/rocket/PKAnimManager.kt
+++ app/src/main/java/com/prance/teacher/features/pk/rocket/PKAnimManager.kt
@@ -9,6 +9,7 @@ import com.prance.lib.common.utils.http.mySubscribe
 import com.prance.teacher.R
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
+import java.util.concurrent.CopyOnWriteArrayList
 
 class PKAnimManager(var context: Context) {
 
@@ -16,7 +17,7 @@ class PKAnimManager(var context: Context) {
     var defaultBackground = GlideApp.with(context).asBitmap().diskCacheStrategy(DiskCacheStrategy.NONE).load(R.drawable.pk_background_0001).submit().get()
 
     //所有火箭
-    var rockets: MutableList<BaseRocket>? = null
+    var rockets: CopyOnWriteArrayList<BaseRocket>? = null
 
     fun getDimen(id: Int): Float {
         return context.resources.getDimensionPixelOffset(id).toFloat()
@@ -34,7 +35,7 @@ class PKAnimManager(var context: Context) {
     init {
 
         Flowable
-                .create<MutableList<BaseRocket>>({
+                .create<CopyOnWriteArrayList<BaseRocket>>({
                     earth = Earth(context)
                     countTime.listeners.add(earth!!)
 
@@ -55,15 +56,16 @@ class PKAnimManager(var context: Context) {
                             .load(R.drawable.big_rocket_shake_00000)
                             .submit().get()
 
-                    it.onNext(mutableListOf(
-                            LittleRocket(context, PointF(getDimen(R.dimen.m224_0), getDimen(R.dimen.m862_0)), PointF(getDimen(R.dimen.m224_0), getDimen(R.dimen.m220_0)), littleRocket),
-                            LittleRocket(context, PointF(getDimen(R.dimen.m386_0), getDimen(R.dimen.m823_0)), PointF(getDimen(R.dimen.m386_0), getDimen(R.dimen.m602_0)), littleRocket),
-                            LittleRocket(context, PointF(getDimen(R.dimen.m542_0), getDimen(R.dimen.m807_0)), PointF(getDimen(R.dimen.m542_0), getDimen(R.dimen.m460_0)), littleRocket),
-                            LittleRocket(context, PointF(getDimen(R.dimen.m1186_0), getDimen(R.dimen.m812_0)), PointF(getDimen(R.dimen.m1186_0), getDimen(R.dimen.m260_0)), littleRocket),
-                            LittleRocket(context, PointF(getDimen(R.dimen.m1346_0), getDimen(R.dimen.m823_0)), PointF(getDimen(R.dimen.m1346_0), getDimen(R.dimen.m682_0)), littleRocket),
-                            LittleRocket(context, PointF(getDimen(R.dimen.m1508_0), getDimen(R.dimen.m862_0)), PointF(getDimen(R.dimen.m1508_0), getDimen(R.dimen.m480_0)), littleRocket),
-                            BigRocket(context, PointF(getDimen(R.dimen.m743_0), getDimen(R.dimen.m580_0)), PointF(getDimen(R.dimen.m743_0), getDimen(R.dimen.m340_0)), bigRocket)
-                    ))
+                    var list = CopyOnWriteArrayList<BaseRocket>()
+                    list.add(LittleRocket(context, PointF(getDimen(R.dimen.m224_0), getDimen(R.dimen.m862_0)), PointF(getDimen(R.dimen.m224_0), getDimen(R.dimen.m220_0)), littleRocket))
+                    list.add(LittleRocket(context, PointF(getDimen(R.dimen.m386_0), getDimen(R.dimen.m823_0)), PointF(getDimen(R.dimen.m386_0), getDimen(R.dimen.m602_0)), littleRocket))
+                    list.add(LittleRocket(context, PointF(getDimen(R.dimen.m542_0), getDimen(R.dimen.m807_0)), PointF(getDimen(R.dimen.m542_0), getDimen(R.dimen.m460_0)), littleRocket))
+                    list.add(LittleRocket(context, PointF(getDimen(R.dimen.m1186_0), getDimen(R.dimen.m812_0)), PointF(getDimen(R.dimen.m1186_0), getDimen(R.dimen.m260_0)), littleRocket))
+                    list.add(LittleRocket(context, PointF(getDimen(R.dimen.m1346_0), getDimen(R.dimen.m823_0)), PointF(getDimen(R.dimen.m1346_0), getDimen(R.dimen.m682_0)), littleRocket))
+                    list.add(LittleRocket(context, PointF(getDimen(R.dimen.m1508_0), getDimen(R.dimen.m862_0)), PointF(getDimen(R.dimen.m1508_0), getDimen(R.dimen.m480_0)), littleRocket))
+                    list.add(BigRocket(context, PointF(getDimen(R.dimen.m743_0), getDimen(R.dimen.m580_0)), PointF(getDimen(R.dimen.m743_0), getDimen(R.dimen.m340_0)), bigRocket))
+
+                    it.onNext(list)
                     it.onComplete()
                 }, BackpressureStrategy.BUFFER)
                 .mySubscribe {
